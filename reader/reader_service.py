ed3eb91b85625b89bddf25ff54c68a7b880763fe
Ian S. Martins <139145782+Ian-S-M@users.noreply.github.com>
2025-07-08 16:29:37 -0300

    Update reader_service.py

--- reader/reader_service.py
+++ reader/reader_service.py
@@ -4,17 +4,23 @@ import boto3
 import uuid
 import os
 
+
 app = FastAPI()
 
 # Inicializa o cliente do Polly
+
 polly = boto3.client('polly', region_name='us-east-1')
 
 @app.post("/speak")
+
 def speak(texto: str = Form(...)):
+    
     # Gera um nome único para o arquivo
+    
     audio_file = f"/tmp/{uuid.uuid4()}.mp3"
 
     # Requisição ao Polly
+    
     response = polly.synthesize_speech(
         Text=texto,
         OutputFormat='mp3',
@@ -23,7 +29,9 @@ def speak(texto: str = Form(...)):
     )
 
     # Salva o áudio retornado
+    
     with open(audio_file, 'wb') as f:
+        
         f.write(response['AudioStream'].read())
 
     return FileResponse(path=audio_file, media_type="audio/mpeg", filename="resposta.mp3")
